10fa3563b6766b65521fb966e9a7ce89f2435ab6
meanwill <48746903+meanwill@users.noreply.github.com>
2020-03-13 15:35:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 echo '修改机器名称'
-sed -i 's/GHOST/GHOST/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/GHOST/g' package/base-files/files/bin/config_generate
 
 #echo '修改网关地址'
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
@@ -10,15 +10,9 @@ sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai
 echo '修改默认主题'
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 
-echo '去除默认bootstrap主题'
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
 echo '删除旧版argon,链接新版'
 rm -rf ./package/lean/luci-theme-argon
 ln -s ../../../luci-theme-argon ./package/lean/
-
-echo '修改wifi名称'
-sed -i 's/OpenWrt/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 echo '修改banner'
 rm -rf package/base-files/files/etc/banner
@@ -30,5 +24,15 @@ git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverch
 echo '集成diy目录'
 ln -s ../../diy ./package/openwrt-packages
 
-
-
+echo '下载openclash'
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+pushd package/luci-app-openclash/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
