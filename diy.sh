68197f445de2827987b0015843c7f1c441f9f32a
Gh-mean <48746903+Gh-mean@users.noreply.github.com>
2021-10-24 10:54:07 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -27,16 +27,3 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 
 echo '集成diy目录'
 ln -s ../../diy ./package/openwrt-packages
-
-echo '下载openclash'
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
-pushd package/luci-app-openclash/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
